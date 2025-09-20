7dc057946d2678c1f82e08267abe47c2afb23a16
krevoor <krevoor@Ubuntu.myguest.virtualbox.org>
2025-09-20 15:17:09 -0600

    updated clean.sh and aesd_qemu_defconfig

--- clean.sh
+++ clean.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-# Clean script for AESD assignment
-# Runs distclean in the buildroot directory
 
-set -e  # exit on first error
 
-# go into buildroot directory
 cd buildroot
 
-# make distclean removes all build products
 make distclean
 
+echo "clean done"
