55a054f80b276868eaa86fda7728b18019e9da04
Karthik Revoor <karthik.r@colorado.edu>
2025-10-12 14:52:02 -0600

    changed the LDD MK file

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,7 +1,7 @@
 LDD_VERSION = 05fa8635d936c0ae1bbaf1c30df5a41d9504e22a
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-KarthikRevoor.git
 LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
+LDD_GIT_SUBMODULES = NO
 
 # Build misc-modules and scull kernel modules
 LDD_MODULE_SUBDIRS = misc-modules
